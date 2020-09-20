385fbad721fc8c9bb77e93958016b3d435c48420
Sohan Janaka <sjanakamandalawallig@lk.imshealth.com>
2020-09-20 22:04:53 +0530

    adding ip

--- controller/utils.php
+++ controller/utils.php
@@ -1,7 +1,5 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
 function getIP(){
 	if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
 		$ip = $_SERVER['HTTP_CLIENT_IP'];
